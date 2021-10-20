62b7636a859079a8fbcd8510543d44febf8ce372
LeeTato <leetato25@gmail.com>
2021-10-20 00:42:37 -0500

    feat(client):services

---
+++ src/client/src/app/services/socket.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Socket } from 'ngx-socket-io';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  public message = '';
+  public username = '';
+  public name = '';
+  public quizzes: any[] = [];
+  public players: any[] = [];
+  public questions: any[] = [];
+  constructor(private socket: Socket, private router: Router) {
+    this.socket.on('connect', () => {});
+    this.router.navigate(['/']);
+    this.socket.on('not unique nickName', (message: string) => {
+      this.message = message;
+    }),
+      this.socket.on('route', (url: string) => {
+        this.router.navigate([url]);
+        console.log('route', url);
+      });
+
+    this.socket.on('Send quizzes', (quizzes: any) => (this.quizzes = quizzes));
+    this.socket.emit('get quizzes');
+
+    this.socket.on(
+      'players-ready',
+      (username: string) => (this.username = username)
+    );
+    this.socket.on('add name', (players: any) => (this.players = players));
+    this.socket.on('player added name',(players:any)=>{this.players=players});
+  }
+  selectQuiz(quizz: any) {
+    this.socket.emit('select quiz', quizz);
+  }
+
+}
