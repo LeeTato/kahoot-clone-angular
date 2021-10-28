17cc0897f6bdeec1e11b7571a621c94f8e3b59b8
LeeTato <leetato25@gmail.com>
2021-10-28 18:55:10 -0500

    ch3

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -10,7 +10,9 @@ import { QuestionBoardPageComponent } from './pages/question-board-page/question
 import { LeaderBoardPageComponent } from './pages/leader-board-page/leader-board-page.component';
 import { FormsModule } from '@angular/forms';
 import { QuizzesComponent } from './pages/quizzes/quizzes.component';
-const config: SocketIoConfig = {url: 'http://localhost:3000', options:{}};
+import { environment } from 'src/environments/environment';
+const config: SocketIoConfig = { url: !environment.production ? 'http://localhost:3000/' : '', options: {} };
+
 
 @NgModule({
   declarations: [
